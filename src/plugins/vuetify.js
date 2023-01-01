aa5ad64710a2553b78df0dd67e0c4e84ec0e999d
HBO30 <diab.4869@gmail.com>
2023-01-01 12:01:38 +0200

    task : colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,32 +4,18 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-      options: { customProperties: true },
-        themes: {
-          dark:{
-            "rich-black": "#090412",
-            "chinese-black": "#0F0A22",
-            "majorelle-blue": "#6546ED",
-            "violets-are-blue-dark": "#7A5BFF",
-            "violets-are-blue-light": "#957CFF",
-            vodka: "#C0B1FF",
-            white: "#FFFFFF",
-            'copy-email-button-once': "#342e31",
-            'fake-path-button-once': "#524e60",
-          
-          },
-          light:{
-            "rich-black": "#090412",
-            "chinese-black": "#0F0A22",
-            "majorelle-blue": "#6546ED",
-            "violets-are-blue-dark": "#7A5BFF",
-            "violets-are-blue-light": "#957CFF",
-            vodka: "#C0B1FF",
-            white: "#FFFFFF",
-            'copy-email-button-once': "#342e31",
-            'fake-path-button-once': "#524e60",
-          }
-        },
-      },
+  theme: {
+    options: { customProperties: true },
+    themes: {
+      light: {
+        primary: '#f3c407',
+        'light-yellow': '#f3dc8d',
+        secondary: '#046188',
+        'light-blue-1': '#649baa',
+        'light-blue-2': '#a1c5d1',
+        "black": '#010101',
+        white: '#ffffff'
+      }
+    },
+  },
 });
