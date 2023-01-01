b8a966ab5a84c5e62812e44cfb02bbd3937be2aa
HBO30 <diab.4869@gmail.com>
2023-01-01 17:05:13 +0200

    bill task

---
+++ src/project/pages/vue/pages/bill/pages/search-bill-page.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-form v-model="valid">
+            <v-card>
+              <v-card-title> Bill </v-card-title>
+              <v-card-sub-title> Phone Number </v-card-sub-title>
+              <v-card-text>
+                <v-text-field solo v-model.number="phoneNumber"> </v-text-field>
+              </v-card-text>
+            </v-card>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      phoneNumber: "",
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/project/router.js
+++ src/project/router.js
@@ -1,18 +1,41 @@
 let project = {
   name: "project",
   path: "",
-  redirect:"project-vue-studio",
-  component:()=> import("./module-layout.vue"),
+  redirect: "project-vue-studio",
+  component: () => import("./module-layout.vue"),
   children: [
     {
       name: "project-vue-studio",
       path: "project-vue-studio",
-      component:()=> import("./layouts/vue/vue-layout.vue"),
+      component: () => import("./layouts/vue/vue-layout.vue"),
       children: [
         {
           path: "",
-          component:()=>   import("./pages/vue/vue-page.vue"),
+          component: () => import("./pages/vue/vue-page.vue"),
+          children: [
+            {
+              path: "bill",
+              component: () => import("./pages/vue/pages/bill/bill-module.vue"),
+              children: [
+                {
+                  path: "",
+                  component: () => import("./pages/vue/pages/bill/pages/search-bill-page"),
+                  children: [
+
+                  ]
+                },
+                {
+                  path: ":number",
+                  component: () => import("./pages/vue/pages/bill/pages/view-bill-page"),
+                  children: [
+
+                  ]
+                }
+              ]
+            },
+          ]
         },
+
       ],
     },
   ],
