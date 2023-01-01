68fff8395ee181c17b10dab9967682517d661744
HBO30 <diab.4869@gmail.com>
2023-01-01 11:54:09 +0200

    task : home page [start]

---
+++ src/project/pages/vue/components/home-preview/home-preview.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main class="yellow">
+      <div class="text-center">
+        <div class="white--text pt-3">
+          <div class="text-h5 mb-3">
+            Ok so this is your studio, you're ready to work now !
+          </div>
+          <p>let's get started</p>
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/project/pages/vue/vue-page.vue
+++ src/project/pages/vue/vue-page.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <local-task-preview></local-task-preview>
+    <local-home-preview></local-home-preview>
   </div>
 </template>
 
 <script>
-import taskPreview from './components/task-preview/task-preview.vue'
+import homePreview from "./components/home-preview/home-preview.vue";
 
 export default {
-  components:{
-    'local-task-preview': taskPreview
-  }
-}
+  components: {
+    "local-home-preview": homePreview,
+  },
+};
 </script>
